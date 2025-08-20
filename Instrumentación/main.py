b641cd6434c0d0c26d25ba666d2e094d1180702c
Waflesa <winstonnarvaez79@gmail.com>
2025-08-20 14:55:05 -0600

    Add files via upload

--- Instrumentación/main.py
+++ Instrumentación/main.py
@@ -9,26 +9,27 @@ sensor_data = {"ph": 0, "nivel": 0, "temp": 0}
 
 # Conectar Arduino
 try:
-    arduino = serial.Serial('COM7', 9600, timeout=1)
-    print("Arduino conectado en COM7")
+    arduino = serial.Serial('COM3', 9600, timeout=1)
+    print("Arduino conectado en COM3")
 except Exception as e:
     arduino = None
     print("No se pudo conectar Arduino:", e)
-
+time.sleep(2)
 # Leer datos en segundo plano
 def read_arduino():
     global sensor_data
     while True:
-        if arduino and arduino.in_waiting:
+        if arduino and arduino.in_waiting > 0:
+            print("Datos recibidos de Arduino:", end=" ")
             line = arduino.readline().decode().strip()
             try:
-                ph, nivel, temp = line.split(":")
+                ph, nivel, temp = line.split(",")
                 sensor_data["ph"] = float(ph)
                 sensor_data["nivel"] = int(nivel)
                 sensor_data["temp"] = float(temp)
             except:
                 pass
-        time.sleep(0.1)
+        time.sleep(2)
 
 if arduino:
     threading.Thread(target=read_arduino, daemon=True).start()
